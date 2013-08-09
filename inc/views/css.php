3fc82f40bcb9a8084464d2c31ab7162773d4851d
Vladimir Anokhin <ano.vladimir@gmail.com>
2013-08-09 18:55:28 +0300

    Custom CSS editor shows a links to actual skin files

--- inc/views/css.php
+++ inc/views/css.php
@@ -1,17 +1,17 @@
 <div id="su-custom-css-screen">
 	<div class="su-custom-css-originals">
 		<strong><?php _e( 'You can see the original styles to override it', $this->textdomain ); ?></strong><br/>
-		<a href="<?php echo $this->assets( 'css', 'content-shortcodes.css' ); ?>">content-shortcodes.css</a>
+		<a href="<?php echo su_skin_url( 'content-shortcodes.css' ); ?>">content-shortcodes.css</a>
 		<span></span>
-		<a href="<?php echo $this->assets( 'css', 'box-shortcodes.css' ); ?>">box-shortcodes.css</a>
+		<a href="<?php echo su_skin_url( 'box-shortcodes.css' ); ?>">box-shortcodes.css</a>
 		<span></span>
-		<a href="<?php echo $this->assets( 'css', 'media-shortcodes.css' ); ?>">media-shortcodes.css</a>
+		<a href="<?php echo su_skin_url( 'media-shortcodes.css' ); ?>">media-shortcodes.css</a>
 		<span></span>
-		<a href="<?php echo $this->assets( 'css', 'galleries-shortcodes.css' ); ?>">galleries-shortcodes.css</a>
+		<a href="<?php echo su_skin_url( 'galleries-shortcodes.css' ); ?>">galleries-shortcodes.css</a>
 		<span></span>
-		<a href="<?php echo $this->assets( 'css', 'players-shortcodes.css' ); ?>">players-shortcodes.css</a>
+		<a href="<?php echo su_skin_url( 'players-shortcodes.css' ); ?>">players-shortcodes.css</a>
 		<span></span>
-		<a href="<?php echo $this->assets( 'css', 'other-shortcodes.css' ); ?>">other-shortcodes.css</a>
+		<a href="<?php echo su_skin_url( 'other-shortcodes.css' ); ?>">other-shortcodes.css</a>
 	</div>
 	<div class="su-custom-css-vars">
 		<strong><?php _e( 'You can use next variables in your custom CSS', $this->textdomain ); ?></strong><br/>
